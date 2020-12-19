d6d9ff1c15bfad0233b27183cfe721c13ad830de
Amybiubiu <1246457100@qq.com>
2020-12-19 09:13:19 +0800

    add sql file

---
+++ sql/secondHand.sql
@@ -0,0 +1,58 @@
+create database secondHand;
+
+use secondHand;
+
+create table users
+(
+    user_id int NOT NULL,
+    user_name char(50) NOT NULL,
+    user_password char(50) NOT NULL,
+    user_token char(255) NULL,
+    primary key (user_id)
+);
+
+create table admins
+(
+    admin_id int NOT NULL,
+    admin_password char(50) NOT NULL,
+    primary key (admin_id)
+);
+
+create table products
+(
+    product_id int not null,
+    product_name char(50) not null,
+    product_class char(50) null,
+    product_pic char(255) null,
+    product_describe char(255) null,
+    product_state boolean not null default 1,
+    product_price int null,
+    product_time timestamp  null ,
+    product_seller int not null,
+    PRIMARY KEY (product_id),
+    FOREIGN KEY (product_seller)
+    REFERENCES users(user_id)
+    
+);
+
+create table buylists
+(
+    user_id int not null primary key,
+    product_id int not null
+);
+
+create table cartlists
+(
+    user_id int not null PRIMARY KEY,
+    product_id int not null
+);
+
+create table comments 
+(
+    product_id int not null PRIMARY KEY,
+    user_id int not null ,
+    comment_detail char(255) null,
+    FOREIGN KEY (user_id)
+    REFERENCES users(user_id)
+);
+
---
+++ sql/sh_data.sql
@@ -0,0 +1,20 @@
+insert into users(user_id, user_name, user_password)
+values(2018214877, "Wendy", "877"),
+(2018214878, "liao", "878"),
+(2018214875, "xf", "875")
+;
+
+insert into products(product_id, product_name, product_seller)
+values(1, "社会学的邀请", 2018214877),
+(2, "认知天性", 2018214878),
+(3, "民俗学", 2018214875);
+
+insert into buylists(user_id, product_id)
+values(2018214877, 2);
+
+insert into cartlists(user_id, product_id)
+values(2018214877, 3);
+
+insert into comments(product_id, user_id, comment_detail)
+values(2, 2018214875, "hly receve comments from 875"),
+(3, 2018214877, "hly comments 875");

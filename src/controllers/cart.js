bfc50947674bdf64d3de7921f1bf251694744dfc
Amybiubiu <1246457100@qq.com>
2020-12-22 21:55:37 +0800

    fix: pass cart api test

--- src/controllers/cart.js
+++ src/controllers/cart.js
@@ -8,16 +8,26 @@ exports.cartList = (req, res) => {
     connection.query(sql, callback(res));
 }
 
-const produceParams = (arr) => {
-    let res = '('
-    for(let i = 0; i < arr.length; i++)
-        res = res + arr[i] + ',';
-    res = res.slice(0, res.length-1) + ')'
-    return res
-}
+// const produceParams = (arr) => {
+//     let res = '('
+//     for(let i = 0; i < arr.length; i++)
+//         res = res + arr[i] + ',';
+//     res = res.slice(0, res.length-1) + ')'
+//     return res
+// }
 exports.cartDel = (req, res) => {
     const {product_id_list} = req.body
-    const param = produceParams(product_id_list)
-    let sql = `delete from cartlists where product_id in ${param}`
+    let sql = `delete from cartlists where product_id in (${product_id_list})`
+    // try{
+    //     connection.query(sql, callback(res))
+    // }catch(error){
+    //     console.error(error);
+    // }
+    connection.query(sql, callback(res))
+}
+
+exports.cartAdd = (req, res) => {
+    const {product_id, user_id} = req.body
+    let sql = `insert into cartlists(user_id, product_id) values(${user_id}, ${product_id})`
     connection.query(sql, callback(res))
 }
--- src/routes/cart.js
+++ src/routes/cart.js
@@ -1,9 +1,9 @@
 const express = require('express')
 const router = express.Router();
 
-const {cartList, cartDel} = require('../controllers/cart');
+const {cartList, cartDel, cartAdd} = require('../controllers/cart');
 
 router.route('/list').get(cartList); // 查询购物车
 router.route('/list').delete(cartDel); // 删除购物车 成批
-// router.route('/add').post(cartAdd);
+router.route('/add').post(cartAdd);
 module.exports = router;
